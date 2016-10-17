f8ee8fefbe646087eba6f2fa07b54b666799f340
chun zheng <zhengchun90@gmail.com>
2016-10-17 14:30:13 -0400

    changed to a new direction

--- Buzz.ino
+++ Buzz.ino
@@ -4,33 +4,19 @@
 void setup() {
   pinMode(11, OUTPUT);
 }
-
+/**
+   Changed it to the song to "Dango Daikazoku"
+   because i'm weeb trash
+   
+   score found here, i used flute becuase it 
+   translates best to 8 bit that i found 
+   http://ichigos.com/res/getfile.php?id=4074&type=pdf&token=36e5cac18a08285cf39432aaed2724ac
+*/
 void loop() {
   firstStanza();
   exit(0);
 }
-/**
-  This is the First stanza in the song "i'm alive" 
-  from the Portal game
-  
-  the score was found here
-  https://www.scribd.com/doc/176716725/I-m-Alive-Next-to-Normal
-*/
+
 void firstStanza() {
-  tone(12, NOTE_G6 , QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_F6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_E6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_E6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_F6, QUARTER); delay((QUARTER + 1) * 10);
-  
-  tone(12, NOTE_A5, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_G6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_F6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_E6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_E6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_E6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_F6, QUARTER + (QUARTER / 2)); delay(HALF + QUARTER + 1);
-  tone(12, NOTE_D6, QUARTER); delay(HALF + 1);
-  tone(12, NOTE_E6, QUARTER); delay(QUARTER + 1);
-  tone(12, NOTE_A5, QUARTER); delay(QUARTER + 1);
+ 
 }
