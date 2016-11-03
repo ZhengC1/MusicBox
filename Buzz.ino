a4c609a5fae8209eb41203a191df638e0f01a91e
chun zheng <zhengchun90@gmail.com>
2016-11-03 13:27:40 -0400

    commit before i delete from desktop:

--- Buzz.ino
+++ Buzz.ino
@@ -4,6 +4,7 @@
 void setup() {
   pinMode(11, OUTPUT);
   firstStanza();
+  secondStanza();
   noTone(11);
 }
 /**
@@ -12,10 +13,17 @@ void setup() {
 
    score found here, i used flute becuase it
    translates best to 8 bit that i found
-   http://ichigos.com/res/getfile.php?id=4074&type=pdf&token=36e5cac18a08285cf39432aaed2724ac
+   http://ichigos.com/res/getfile.php?id=4074&type=pdf&token=36e6cac18a08286cf39432aaed2724ac
 */
 void loop() {
 
+}
+
+void runStanza(int* notes, int* durations, int len) {
+  for (int i = 0; i < len; i++) {
+    tone(11, notes[i], durations[i]);
+    delay(durations[i] + 1);
+  } 
 }
 
 /**
@@ -26,9 +34,9 @@ E -> DS
 */
 void chorus() {
   int notes[LEN] = {
-    NOTE_F5, NOTE_DS5, NOTE_GS5, NOTE_GS5, NOTE_AS5,
-    NOTE_AS5, NOTE_C6, NOTE_GS5, NOTE_DS5,
-    NOTE_F5, NOTE_DS5, NOTE_GS5, NOTE_GS5, NOTE_AS5
+    NOTE_F6, NOTE_DS6, NOTE_GS6, NOTE_GS6, NOTE_AS6,
+    NOTE_AS6, NOTE_C7, NOTE_GS6, NOTE_DS6,
+    NOTE_F6, NOTE_DS6, NOTE_GS6, NOTE_GS6, NOTE_AS6
   };
 
   int duration[LEN] = {
@@ -43,22 +51,42 @@ void chorus() {
 void firstStanza() {
   chorus();
   int notes[4] = {
-    NOTE_F5,
-    NOTE_GS5, 0 , 0
+    NOTE_F6,
+    NOTE_GS6, 0 , 0
   };
 
   int duration[4] = {
-    EIGHTH,
-    QUARTER, QUARTER, HALF
+    EIGHTH, QUARTER, QUARTER, HALF
   };
 
   runStanza(notes, duration, 4); 
   chorus();
 }
 
-void runStanza(int* notes, int* durations, int len) {
-  for (int i = 0; i < len; i++) {
-    tone(11, notes[i], durations[i]);
-    delay(durations[i] + 1);
-  } 
+void secondStanza() {
+  int notes[4] = {
+     NOTE_AS6, NOTE_C7, NOTE_AS6, 0
+  };
+  
+  int duration[4] = {
+    QUARTER, QUARTER, QUARTER, QUARTER
+  };
+  
+  runStanza(notes, duration, 4);
+  
+  chorus();
+  
+  int note[15] = {
+    NOTE_GS6, 0, NOTE_GS6,
+    NOTE_C7, NOTE_CS7, NOTE_GS6, NOTE_F6, NOTE_F6, NOTE_GS6, NOTE_AS6, NOTE_F6,
+    NOTE_GS6, NOTE_DS6, NOTE_F6, NOTE_DS6
+            
+  };
+  int durations[15] = {
+    HALF, HALF, HALF,
+    EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH, EIGHTH,
+    QUARTER, QUARTER, QUARTER, QUARTER
+  };
+  
+  runStanza(note, durations, 15); 
 }
